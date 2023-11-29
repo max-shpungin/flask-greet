0afe574623473027a01a4ac5f41f0cf259b4e397
Eric Huie <erichuie97@gmail.com>
2023-11-29 12:35:21 -0800

    added routes for calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,21 @@
 # Put your app in here.
+from flask import Flask, request
+import operations
+
+app = Flask(__name__)
+
+@app.get("/add")
+def add_endpoint():
+    return str(operations.add(int(request.args["a"]), int(request.args["b"])))
+
+@app.get("/sub")
+def sub_endpoint():
+    return str(operations.sub(int(request.args["a"]), int(request.args["b"])))
+
+@app.get("/mult")
+def mult_endpoint():
+    return str(operations.mult(int(request.args["a"]), int(request.args["b"])))
+
+@app.get("/div")
+def div_endpoint():
+    return str(operations.div(int(request.args["a"]), int(request.args["b"])))
